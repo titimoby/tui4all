77fe872cea048bf5df4ffe211ea1da8fb3d77797
Thierry Chantier <thierry.chantier@dataiku.com>
2025-10-01 11:48:10 +0200

    improve first draft of java demo
    add a comment in app properties to show how to remove banner
    add basic suport of a table in tui-app
    follow IntelliJ suggestions on some warnings

--- java/tui/src/main/java/com/dataiku/devadv/tui/view/TuiAppViewBuilder.java
+++ java/tui/src/main/java/com/dataiku/devadv/tui/view/TuiAppViewBuilder.java
@@ -3,8 +3,6 @@ package com.dataiku.devadv.tui.view;
 import com.dataiku.devadv.tui.data.SampleData;
 import org.springframework.shell.component.view.control.*;
 import org.springframework.shell.component.view.event.KeyEvent;
-import org.springframework.shell.geom.HorizontalAlign;
-import org.springframework.shell.geom.VerticalAlign;
 import org.springframework.shell.table.ArrayTableModel;
 import org.springframework.shell.table.BorderStyle;
 import org.springframework.shell.table.TableBuilder;
@@ -13,47 +11,48 @@ import org.springframework.shell.table.TableModel;
 import java.util.List;
 
 public class TuiAppViewBuilder {
-  public AppView createTuiAppView() {
 
-    MenuBarView menuBar = createMenuBarView();
-    StatusBarView statusBar = createStatusBarView();
-    BoxView main = createMainView();
+    public AppView createTuiAppView() {
 
-    AppView view = new AppView(
-            main, menuBar, statusBar
-    );
-    return view;
-  }
+        MenuBarView menuBar = createMenuBarView();
+        StatusBarView statusBar = createStatusBarView();
+        ListView main = createMainView();
 
-  private static BoxView createMainView() {
-    TableModel model = new ArrayTableModel(SampleData.getAnimals());
-    TableBuilder tableBuilder = new TableBuilder(model);
-    tableBuilder.addFullBorder(BorderStyle.fancy_light_triple_dash);
+        return new AppView(
+                main, menuBar, statusBar
+        );
+    }
 
-    BoxView main = new BoxView();
-    main.setDrawFunction((screen, rect) -> {
-      screen.writerBuilder().build().text(tableBuilder.build().render(80), rect, HorizontalAlign.CENTER, VerticalAlign.CENTER);
-      return rect;
-    });
-    return main;
-  }
+    private static ListView createMainView() {
+        TableModel model = new ArrayTableModel(SampleData.getAnimals());
+        TableBuilder tableBuilder = new TableBuilder(model);
+        tableBuilder.addFullBorder(BorderStyle.fancy_light_triple_dash);
 
-  private static StatusBarView createStatusBarView() {
-    StatusBarView.StatusItem item1 = new StatusBarView.StatusItem("Item1");
-    StatusBarView statusBar = new StatusBarView(List.of(item1));
-    return statusBar;
-  }
+        ListView main = new ListView();
+        String tableRendered = tableBuilder.build().render(80);
+        String[] lines = tableRendered.split("\n");
 
-  private static MenuBarView createMenuBarView() {
-    Runnable quitAction = () -> {};
-    Runnable aboutAction = () -> {};
-    MenuBarView menuBar = MenuBarView.of(
-            MenuBarView.MenuBarItem.of("File",
-                            MenuView.MenuItem.of("Quit", MenuView.MenuItemCheckStyle.NOCHECK, quitAction))
-                    .setHotKey(KeyEvent.Key.f | KeyEvent.KeyMask.AltMask),
-            MenuBarView.MenuBarItem.of("Help",
-                    MenuView.MenuItem.of("About", MenuView.MenuItemCheckStyle.NOCHECK, aboutAction))
-    );
-    return menuBar;
-  }
+        main.setItems(List.of(lines));
+
+        return main;
+    }
+
+    private static StatusBarView createStatusBarView() {
+        StatusBarView.StatusItem item1 = new StatusBarView.StatusItem("Item1");
+        return new StatusBarView(List.of(item1));
+    }
+
+    private static MenuBarView createMenuBarView() {
+        Runnable quitAction = () -> {
+        };
+        Runnable aboutAction = () -> {
+        };
+        return MenuBarView.of(
+                MenuBarView.MenuBarItem.of("File",
+                                MenuView.MenuItem.of("Quit", MenuView.MenuItemCheckStyle.NOCHECK, quitAction))
+                        .setHotKey(KeyEvent.Key.f | KeyEvent.KeyMask.AltMask),
+                MenuBarView.MenuBarItem.of("Help",
+                        MenuView.MenuItem.of("About", MenuView.MenuItemCheckStyle.NOCHECK, aboutAction))
+        );
+    }
 }
